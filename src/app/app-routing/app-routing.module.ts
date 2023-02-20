0ff1da9b81a4a70c2ce0b42d68df103704e07163
Dumitru Frunza <crme009@cedmd.ced.it>
2023-02-20 12:17:38 +0200

    Generated Routing Module

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Routes} from "@angular/router";
+
+
+
+const appRotues: Routes = []
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    appRotues
+  ]
+})
+export class AppRoutingModule { }
